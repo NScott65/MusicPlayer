e0ffdd65aa524b214f11bcb9c1861dadf41d1fb1
NScott65 <144457750+NScott65@users.noreply.github.com>
2024-04-17 09:45:17 -0400

    h

--- src/MusicPlayer.java
+++ src/MusicPlayer.java
@@ -1,12 +1,24 @@
+import javax.sound.sampled.*;
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
+import java.io.IOException;
 
 public class MusicPlayer {
+    //Class Variables
+    private Clip clip;
+    private Timer timer;
+    private JLabel currentTime;
+    private JLabel totalTime;
 
     //constructor - building the music player
     public MusicPlayer(){
+
         //Song Options
-        String[] songList = {"GORG.wav", "Code Kings.wave"};
+        String[] songList = {"GORG.wav", "Code Kings.wav","Dumbbells.wav","Fire in My Belly.wav","For Loop.wav", "GUI Mastermind.wav", "Hashin_in the Code.wav",
+        "Hey Papi.wav","Mr.Scott.wav","Programming.wav","The Boolean Blues.wav","The Codebreaker_s Fury.wav"};
 
         //Frame
         JFrame frame = new JFrame("Music Player");
@@ -27,8 +39,8 @@ public class MusicPlayer {
 
         //labels
         JPanel labelPanel = new JPanel();
-        JLabel currentTime = new JLabel("Current Time: 0");
-        JLabel totalTime = new JLabel("Total Time: 0");
+        currentTime = new JLabel("Current Time: 0");
+        totalTime = new JLabel("Total Time: 0");
         labelPanel.add(currentTime);
         labelPanel.add(totalTime);
 
@@ -36,6 +48,25 @@ public class MusicPlayer {
         JPanel dropDown = new JPanel();
         JComboBox<String> songSelector = new JComboBox<>(songList);
         dropDown.add(songSelector);
+
+        //Button Function
+        playButton.addActionListener(new ActionListener(){
+            public void actionPerformed(ActionEvent event){
+                playMusic(songSelector.getSelectedItem().toString());
+                startTimer();
+                updateLabels();
+            }
+        });
+
+        pauseButton.addActionListener(new ActionListener(){
+            public void actionPerformed(ActionEvent event){
+                pauseMusic(songSelector.getSelectedItem().toString());
+                stopTimer();
+                updateLabels();
+            }
+        });
+
+
 
         //Main panel Order
         panel.add(dropDown);
@@ -45,6 +76,50 @@ public class MusicPlayer {
         //Main frame visible
         frame.add(panel);
         frame.setVisible(true);
+    }
+
+    public void playMusic(String selectedSong){
+
+        try{
+           File file = new File(System.getProperty("user.dir") + "\\src\\audio\\" + selectedSong);
+            AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(file);
+            clip = AudioSystem.getClip();
+            clip.open(audioInputStream);
+            clip.start();
+        }
+        catch(IOException | UnsupportedAudioFileException | LineUnavailableException e){
+            e.printStackTrace();
+        }
+    }
+
+    public void pauseMusic(String selectedSong){
+        if(clip != null && clip.isRunning()){
+            clip.stop();
+        }
+    }
+
+    public void startTimer(){
+        timer = new Timer(1000, new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                updateLabels();
+            }
+        });
+        timer.start();
+    }
+
+    public void updateLabels(){
+        if(clip != null && clip.isOpen()){
+            if (clip.isRunning()){
+                currentTime.setText("Current Time: " + clip.getMicrosecondPosition() / 1000000 + "s");
+                totalTime.setText("Total Time: " + clip.getMicrosecondLength() / 1000000 + "s");
+            }
+        }
+    }
+
+    public void stopTimer(){
+        if(timer != null){
+            timer.stop();
+        }
     }
 
     public static void main(String[] args) {
